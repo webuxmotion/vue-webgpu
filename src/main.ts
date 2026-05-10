32635a1d4aabcb9b1a29eb06541cd7cb3aed786b
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2026-05-10 18:57:30 +0300

    Draw triangle

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,63 @@
-import './style.css'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import shader from "./shaders.wgsl";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <button id="counter" type="button" class="counter"></button>
-  
-</section>
+const Initialize = async () => {
+  if (!navigator.gpu) throw new Error("WebGPU not supported");
 
+  const canvas = document.getElementById("gfx-main") as HTMLCanvasElement;
 
-<section id="spacer"></section>
-`
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) throw new Error("No GPUAdapter found");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const device: GPUDevice = await adapter.requestDevice();
+
+  const context = canvas.getContext("webgpu") as GPUCanvasContext;
+
+  const format: GPUTextureFormat = navigator.gpu.getPreferredCanvasFormat();
+
+  context.configure({ device, format, alphaMode: "opaque" });
+
+  const pipeline: GPURenderPipeline = device.createRenderPipeline({
+    layout: "auto",
+    vertex: {
+      module: device.createShaderModule({
+        code: shader,
+      }),
+      entryPoint: "vs_main",
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: shader,
+      }),
+      entryPoint: "fs_main",
+      targets: [
+        {
+          format: format,
+        },
+      ],
+    },
+    primitive: {
+      topology: "triangle-list",
+    },
+  });
+
+  const commandEncoder: GPUCommandEncoder = device.createCommandEncoder();
+  const textureView: GPUTextureView = context.getCurrentTexture().createView();
+  const renderPass: GPURenderPassEncoder = commandEncoder.beginRenderPass({
+    colorAttachments: [
+      {
+        view: textureView,
+        clearValue: { r: 0.5, g: 0.0, b: 0.25, a: 1.0 },
+        loadOp: "clear",
+        storeOp: "store",
+      },
+    ],
+  });
+  renderPass.setPipeline(pipeline);
+  renderPass.draw(3, 1, 0, 0);
+  renderPass.end();
+
+  device.queue.submit([commandEncoder.finish()]);
+};
+
+Initialize();
